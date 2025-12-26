27405ef5d05dce46b5d794c13518fe2cc7d353ed
ENAANAI <aenaanai@uae.ac.ma>
2025-12-26 09:45:15 +0100

    create user

---
+++ src/main/java/com/uae/pagination/entities/User.java
@@ -0,0 +1,46 @@
+package com.uae.pagination.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor @AllArgsConstructor @Data
+public class User implements UserDetails {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String username;
+    String password;
+    String roles;
+    boolean active;
+
+    private List<String> getRoles(){
+        return Arrays.asList(this.roles.split(","));
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        ArrayList<GrantedAuthority> authorities = new ArrayList<>();
+        getRoles().forEach(role -> {
+            authorities.add(new SimpleGrantedAuthority(role));
+        });
+        return authorities;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return active;
+    }
+}
